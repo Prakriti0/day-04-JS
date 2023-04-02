5a5bd47f5518bf50bc610d9eb8feec395459917e
Prakriti0 <prakritilimbu79@gmail.com>
2023-04-02 13:51:33 +0545

    first commit

---
+++ home.js
@@ -0,0 +1,12 @@
+var p = document.getElementById("p");
+var q = document.getElementById("q");
+var r = document.getElementById("r");
+p.addEventListener("click", function(){
+    document.body.style.backgroundColor="red";
+})
+q.addEventListener("click", function(){
+    document.body.style.backgroundColor="black";
+})
+r.addEventListener("click",function(){
+    document.body.style.backgroundColor="blue";
+})
---
+++ script.js
@@ -0,0 +1,6 @@
+var xyz = document.getElementById("xyz");
+xyz.innerHTML= "<b>Hello</b>";
+var abc = document.getElementsByClassName("abc");
+abc[0].innerHTML="Goodafternoon";
+xyz.style.color="red";
+abc[0].style.color="blue";
